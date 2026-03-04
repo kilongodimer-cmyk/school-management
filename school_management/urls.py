ed9bfe697f0455280d07ed82331abf3efeb34f14
User <user@example.com>
2026-03-04 23:50:57 +0100

    Add home page

--- school_management/urls.py
+++ school_management/urls.py
@@ -3,10 +3,12 @@ URL configuration for school_management project.
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('apps.accounts.urls', namespace='accounts')),
     path('students/', include('apps.students.urls', namespace='students')),
