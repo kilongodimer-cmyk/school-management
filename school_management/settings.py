ab185199634a3af29c2efe8f0733d161d7a79eed
User <user@example.com>
2026-03-05 15:33:34 +0100

    Redesign home page

--- school_management/settings.py
+++ school_management/settings.py
@@ -10,7 +10,7 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='Dimer@07')
+SECRET_KEY = config('SECRET_KEY', default='MyS3cureP@ssw0rd!')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
